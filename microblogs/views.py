e8f2de7aef78719145d46a6863d720ef721d6098
Zebin Liao <zebin.liao@kcl.ac.uk>
2021-10-25 08:46:14 +0100

    Add flash error message after unsuccessful log in

--- microblogs/views.py
+++ microblogs/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from .forms import PostForm
 from .forms import LogInForm
 from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 def feed(request):
     form = PostForm()
@@ -20,6 +21,7 @@ def log_in(request):
             if user is not None:
                 login(request, user)
                 return redirect('feed')
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
     form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
 
